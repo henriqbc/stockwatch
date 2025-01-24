25c3aaa3bd520f30e3cc6fbd00895bbb1a84856b
Henrique <henrique.bovo@usp.br>
2025-01-24 14:59:28 -0300

    Adding models

--- stockwatch/stocks/models.py
+++ stockwatch/stocks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+MAX_STR_FIELD_SIZE = 150
+
+class MonitoredStock(models.Model):
+    name = models.CharField(max_length=MAX_STR_FIELD_SIZE, default='BR1')
+    upper_tunnel_bound = models.IntegerField()
+    lower_tunnel_bound = models.IntegerField()
+    periodicity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class StockUpdate(models.Model):
+    stock_id = models.ForeignKey("MonitoredStock", on_delete=models.CASCADE)
+    price = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
